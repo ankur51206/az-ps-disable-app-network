c9f5bb1fc6f75e5ecffffdf27c39fccd3d7fd7fe
ankurvc <ankur.patel@viitor.cloud>
2023-05-17 07:45:31 +0530

    update script

--- s1.ps1
+++
@@ -1,5 +0,0 @@
-Connect-AzAccount
-
-foreach ($resourceId in Get-Content .\file.txt) {
-   az webapp config access-restriction show --ids $webAppId --query "ipSecurityRestrictions" --output table
-}
