d214910985eb14efadd38cdb03f88b4e5d64056e
ankur51206 <32640688+ankur51206@users.noreply.github.com>
2023-05-17 07:05:19 +0530

    Update s2.ps1

--- s2.ps1
+++ s2.ps1
@@ -1,3 +1,1 @@
-foreach ($resourceId in Get-Content .\file.txt) {
-    az webapp config access-restriction add --ids $resourceId --rule-name 'IP example rule' --action Allow --ip-address 0.0.0.0/0 --priority 100
-}
+
